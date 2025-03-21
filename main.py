10f166b1d92211ff6ed8bacb57b479cfd37805ac
thisismyclass <hudsonfolson77@gmail.com>
2025-03-21 11:00:05 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -6,21 +6,45 @@ import pandas as pd
 
 # Load in the data with read_csv()
 # TODO #1: change the file name to your data file name
-temp_data = pd.read_csv("temperature_data.csv", header=0)   # identify the header row
-
+temp_data = pd.read_csv("Brooklyn.csv", header=0)   # identify the header row
+data = pd.read_csv("EastNewYork.csv", header=0)
+'''
 # TODO #2: Use matplotlib to make a line graph
-plt.plot(temp_data['Year'], temp_data['Anomaly'], color='gray')
-plt.ylabel('Temperature Anomalies in Celsius')
-plt.xlabel('Years')
-plt.title('Change in Temperatures')
+plt.plot(temp_data['StartDate'], temp_data['DataValue'], color='gray')
+plt.ylabel('Air Quality')
+plt.xlabel('DATE')
+plt.title('Changes in Air Quality in brooklyn')
 
+
+plt.plot(data['StartDate'], data['DataValue'], color='blue')
+plt.ylabel('Air Quality')
+plt.xlabel('DATE')
+plt.title('Changes in Air Quality in EastNewYork')
+'''
+
+
+
+
+
+
+fig, axs = plt.subplots(1, 2, figsize=(9, 3), sharey=True)
+axs[0].plot(temp_data['StartDate'], temp_data['DataValue'])
+plt.ylabel('Air Quality')
+plt.xlabel('DATE')
+axs[1].plot(data['StartDate'], data['DataValue'])
+plt.ylabel('Air Quality')
+plt.xlabel('DATE')
+fig.suptitle('       Brooklyn                  V.S               EastNewYork')
+plt.show()
+
+
+
+
+'''
 
 # TODO #3: Plot LOWESS in a line graph
 plt.plot(temp_data['Year'], temp_data['LOWESS'], color='blue')
-plt.ylabel('Temperature Anomalies in Celsius')
-plt.xlabel('Years')
-plt.title('Change in Temperatures')
-#plt.show()
+
 
 # TODO #4: Use matplotlib to make a bar chart
 plt.bar(temp_data['Year'], temp_data['Anomaly'], align='center', color='green')
@@ -48,3 +72,4 @@ for i in range(len(temp_data['Anomaly'])):
 print("The maximum anomaly is:", max_anomaly, "which occured in", max_year)
 print("The minimum anomaly is:", min_anomaly, "which occured in", min_year)
 print("The average anomaly is:", avg_anomaly)
+'''
